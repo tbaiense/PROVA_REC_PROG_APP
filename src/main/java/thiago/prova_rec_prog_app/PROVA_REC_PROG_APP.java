6e94c059e38132222d261665b4ebae79eecd07e9
t.baiense <thiago.baiense@aluno.senai.br>
2024-11-13 18:57:37 -0300

    adiciona arquivos iniciais

---
+++ src/main/java/thiago/prova_rec_prog_app/PROVA_REC_PROG_APP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package thiago.prova_rec_prog_app;
+
+/**
+ *
+ * @author t.baiense
+ */
+public class PROVA_REC_PROG_APP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
